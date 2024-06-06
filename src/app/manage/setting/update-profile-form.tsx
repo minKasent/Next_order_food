9ea5b87c3e535d884420a9f0db11cb4614e73886
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-06 14:32:47 +0700

    Hiển thị thông tin profile và preview avatar

--- src/app/manage/setting/update-profile-form.tsx
+++ src/app/manage/setting/update-profile-form.tsx
@@ -5,12 +5,20 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Upload } from 'lucide-react'
 import { useForm } from 'react-hook-form'
-import { UpdateMeBody, UpdateMeBodyType } from '@/schemaValidations/account.schema'
+import {
+  UpdateMeBody,
+  UpdateMeBodyType
+} from '@/schemaValidations/account.schema'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { useAccountProfile } from '@/queries/useAccount'
 
 export default function UpdateProfileForm() {
+  const [file, setFile] = useState<File | null>(null)
+  const avatarInputRef = useRef<HTMLInputElement>(null)
+  const { data } = useAccountProfile()
   const form = useForm<UpdateMeBodyType>({
     resolver: zodResolver(UpdateMeBody),
     defaultValues: {
@@ -19,9 +27,31 @@ export default function UpdateProfileForm() {
     }
   })
 
+  const avatar = form.watch('avatar')
+  const name = form.watch('name')
+  useEffect(() => {
+    if (data) {
+      const { name, avatar } = data.payload.data
+      form.reset({
+        name,
+        avatar: avatar ?? ''
+      })
+    }
+  }, [form, data])
+  // Nếu các bạn dùng Next.js 15 (tức React 19) thì không cần dùng useMemo chỗ này
+  const previewAvatar = useMemo(() => {
+    if (file) {
+      return URL.createObjectURL(file)
+    }
+    return avatar
+  }, [avatar, file])
+
   return (
     <Form {...form}>
-      <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8'>
+      <form
+        noValidate
+        className='grid auto-rows-max items-start gap-4 md:gap-8'
+      >
         <Card x-chunk='dashboard-07-chunk-0'>
           <CardHeader>
             <CardTitle>Thông tin cá nhân</CardTitle>
@@ -35,13 +65,27 @@ export default function UpdateProfileForm() {
                   <FormItem>
                     <div className='flex gap-2 items-start justify-start'>
                       <Avatar className='aspect-square w-[100px] h-[100px] rounded-md object-cover'>
-                        <AvatarImage src={'Duoc'} />
-                        <AvatarFallback className='rounded-none'>{'duoc'}</AvatarFallback>
+                        <AvatarImage src={previewAvatar} />
+                        <AvatarFallback className='rounded-none'>
+                          {name}
+                        </AvatarFallback>
                       </Avatar>
-                      <input type='file' accept='image/*' className='hidden' />
+                      <input
+                        type='file'
+                        accept='image/*'
+                        className='hidden'
+                        ref={avatarInputRef}
+                        onChange={(e) => {
+                          const file = e.target.files?.[0]
+                          if (file) {
+                            setFile(file)
+                          }
+                        }}
+                      />
                       <button
                         className='flex aspect-square w-[100px] items-center justify-center rounded-md border border-dashed'
                         type='button'
+                        onClick={() => avatarInputRef.current?.click()}
                       >
                         <Upload className='h-4 w-4 text-muted-foreground' />
                         <span className='sr-only'>Upload</span>
@@ -58,7 +102,12 @@ export default function UpdateProfileForm() {
                   <FormItem>
                     <div className='grid gap-3'>
                       <Label htmlFor='name'>Tên</Label>
-                      <Input id='name' type='text' className='w-full' {...field} />
+                      <Input
+                        id='name'
+                        type='text'
+                        className='w-full'
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>

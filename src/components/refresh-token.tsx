2a99ff79b1a27e457630fd9fe0cf459c97074a40
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-12 14:00:06 +0700

    Đang dùng thì refresh token hết hạn

--- src/components/refresh-token.tsx
+++ src/components/refresh-token.tsx
@@ -1,13 +1,14 @@
 'use client'
 
 import { checkAndRefreshToken } from '@/lib/utils'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 // Những page sau sẽ không check refesh token
 const UNAUTHENTICATED_PATH = ['/login', '/logout', '/refresh-token']
 export default function RefreshToken() {
   const pathname = usePathname()
+  const router = useRouter()
   useEffect(() => {
     if (UNAUTHENTICATED_PATH.includes(pathname)) return
     let interval: any = null
@@ -15,15 +16,25 @@ export default function RefreshToken() {
     checkAndRefreshToken({
       onError: () => {
         clearInterval(interval)
+        router.push('/login')
       }
     })
     // Timeout interval phải bé hơn thời gian hết hạn của access token
     // Ví dụ thời gian hết hạn access token là 10s thì 1s mình sẽ cho check 1 lần
     const TIMEOUT = 1000
-    interval = setInterval(checkAndRefreshToken, TIMEOUT)
+    interval = setInterval(
+      () =>
+        checkAndRefreshToken({
+          onError: () => {
+            clearInterval(interval)
+            router.push('/login')
+          }
+        }),
+      TIMEOUT
+    )
     return () => {
       clearInterval(interval)
     }
-  }, [pathname])
+  }, [pathname, router])
   return null
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -55,7 +55,10 @@ export const setAccessTokenToLocalStorage = (value: string) =>
 
 export const setRefreshTokenToLocalStorage = (value: string) =>
   isBrowser && localStorage.setItem('refreshToken', value)
-
+export const removeTokensFromLocalStorage = () => {
+  isBrowser && localStorage.removeItem('accessToken')
+  isBrowser && localStorage.removeItem('refreshToken')
+}
 export const checkAndRefreshToken = async (param?: {
   onError?: () => void
   onSuccess?: () => void
@@ -78,8 +81,11 @@ export const checkAndRefreshToken = async (param?: {
   // Thời điểm hết hạn của token là tính theo epoch time (s)
   // Còn khi các bạn dùng cú pháp new Date().getTime() thì nó sẽ trả về epoch time (ms)
   const now = Math.round(new Date().getTime() / 1000)
-  // trường hợp refresh token hết hạn thì không xử lý nữa
-  if (decodedRefreshToken.exp <= now) return
+  // trường hợp refresh token hết hạn thì cho logout
+  if (decodedRefreshToken.exp <= now) {
+    removeTokensFromLocalStorage()
+    return param?.onError && param.onError()
+  }
   // Ví dụ access token của chúng ta có thời gian hết hạn là 10s
   // thì mình sẽ kiểm tra còn 1/3 thời gian (3s) thì mình sẽ cho refresh token lại
   // Thời gian còn lại sẽ tính dựa trên công thức: decodedAccessToken.exp - now

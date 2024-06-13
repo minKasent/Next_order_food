2d36bda74ae9e4e476e5998e5affda0545ded00e
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-13 20:44:06 +0700

    Khai báo API cho account

--- src/app/api/auth/refresh-token/route.ts
+++ src/app/api/auth/refresh-token/route.ts
@@ -42,6 +42,7 @@ export async function POST(request: Request) {
     })
     return Response.json(payload)
   } catch (error: any) {
+    console.log(error)
     return Response.json(
       {
         message: error.message ?? 'Có lỗi xảy ra'

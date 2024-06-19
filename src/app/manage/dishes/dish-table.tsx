6e30cc5c412475334a4aca6d5e5b0a768b01b0d8
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-19 14:49:01 +0700

    Xóa món ăn

--- src/app/manage/dishes/dish-table.tsx
+++ src/app/manage/dishes/dish-table.tsx
@@ -44,13 +44,18 @@ import {
   AlertDialogHeader,
   AlertDialogTitle
 } from '@/components/ui/alert-dialog'
-import { formatCurrency, getVietnameseDishStatus } from '@/lib/utils'
+import {
+  formatCurrency,
+  getVietnameseDishStatus,
+  handleErrorApi
+} from '@/lib/utils'
 import { useSearchParams } from 'next/navigation'
 import AutoPagination from '@/components/auto-pagination'
 import { DishListResType } from '@/schemaValidations/dish.schema'
 import EditDish from '@/app/manage/dishes/edit-dish'
 import AddDish from '@/app/manage/dishes/add-dish'
-import { useDishListQuery } from '@/queries/useDish'
+import { useDeleteDishMutation, useDishListQuery } from '@/queries/useDish'
+import { toast } from '@/components/ui/use-toast'
 
 type DishItem = DishListResType['data'][0]
 
@@ -155,6 +160,22 @@ function AlertDialogDeleteDish({
   dishDelete: DishItem | null
   setDishDelete: (value: DishItem | null) => void
 }) {
+  const { mutateAsync } = useDeleteDishMutation()
+  const deleteDish = async () => {
+    if (dishDelete) {
+      try {
+        const result = await mutateAsync(dishDelete.id)
+        setDishDelete(null)
+        toast({
+          title: result.payload.message
+        })
+      } catch (error) {
+        handleErrorApi({
+          error
+        })
+      }
+    }
+  }
   return (
     <AlertDialog
       open={Boolean(dishDelete)}
@@ -177,7 +198,7 @@ function AlertDialogDeleteDish({
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction>Continue</AlertDialogAction>
+          <AlertDialogAction onClick={deleteDish}>Continue</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>

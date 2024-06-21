5a9c2c97f8ebb5ae3c35b339f293a498b5372330
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-21 15:05:55 +0700

    Thêm thông tin vào QR Code canvas

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -14,19 +14,45 @@ export default function QRCodeTable({
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   useEffect(() => {
-    const canvas = canvasRef.current
-
+    // Hiện tại: Thư viện QRCode nó sẽ vẽ lên cái thẻ Canvas
+    // Bây giờ: Chúng ta sẽ tạo 1 cái thẻ canvas ảo để thư viện QRCode code nó vẽ QR lên trên đó.
+    // Và chúng ta sẽ edit thẻ canvas thật
+    // Cuối cùng thì chúng ta sẽ đưa cái thẻ canvas ảo chứa QR Code ở trên vào thẻ Canvas thật
+    const canvas = canvasRef.current!
+    canvas.height = width + 70
+    canvas.width = width
+    const canvasContext = canvas.getContext('2d')!
+    canvasContext.fillStyle = '#fff'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.font = '20px Arial'
+    canvasContext.textAlign = 'center'
+    canvasContext.fillStyle = '#000'
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    )
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    )
+    const virtalCanvas = document.createElement('canvas')
     QRCode.toCanvas(
-      canvas,
+      virtalCanvas,
       getTableLink({
         token,
         tableNumber
       }),
+      {
+        width,
+        margin: 4
+      },
       function (error) {
         if (error) console.error(error)
-        console.log('success!')
+        canvasContext.drawImage(virtalCanvas, 0, 0, width, width)
       }
     )
-  }, [token, tableNumber])
+  }, [token, width, tableNumber])
   return <canvas ref={canvasRef} />
 }

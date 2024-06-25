0c9255019b541b4baa0716af124fa8dde76ac761
Dư Thanh Được <duthanhduoc@gmail.com>
2024-06-25 21:50:31 +0700

    Fetch danh sách món ăn ở trang chủ

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+        pathname: '/**'
+      }
+    ]
+  }
+}
 
-export default nextConfig;
+export default nextConfig

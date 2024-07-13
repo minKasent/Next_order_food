773127fc25ff23082f65d3e0e9a062e57b8655de
Dư Thanh Được <duthanhduoc@gmail.com>
2024-07-13 22:54:36 +0700

    Code logic tạo đơn hàng

--- src/app/manage/orders/guests-dialog.tsx
+++ src/app/manage/orders/guests-dialog.tsx
@@ -1,6 +1,19 @@
 import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from '@/components/ui/table'
 import AutoPagination from '@/components/auto-pagination'
 import { useEffect, useState } from 'react'
 import {
@@ -19,6 +32,7 @@ import { formatDateTimeToLocaleString, simpleMatchText } from '@/lib/utils'
 import { Input } from '@/components/ui/input'
 import { GetListGuestsResType } from '@/schemaValidations/account.schema'
 import { endOfDay, format, startOfDay } from 'date-fns'
+import { useGetGuestListQuery } from '@/queries/useAccount'
 
 type GuestItem = GetListGuestsResType['data'][0]
 
@@ -33,13 +47,25 @@ export const columns: ColumnDef<GuestItem>[] = [
     ),
     filterFn: (row, columnId, filterValue: string) => {
       if (filterValue === undefined) return true
-      return simpleMatchText(row.original.name + String(row.original.id), String(filterValue))
+      return simpleMatchText(
+        row.original.name + String(row.original.id),
+        String(filterValue)
+      )
     }
   },
   {
     accessorKey: 'tableNumber',
     header: 'Số bàn',
-    cell: ({ row }) => <div className='capitalize'>{row.getValue('tableNumber')}</div>
+    cell: ({ row }) => (
+      <div className='capitalize'>{row.getValue('tableNumber')}</div>
+    ),
+    filterFn: (row, columnId, filterValue: string) => {
+      if (filterValue === undefined) return true
+      return simpleMatchText(
+        String(row.original.tableNumber),
+        String(filterValue)
+      )
+    }
   },
   {
     accessorKey: 'createdAt',
@@ -56,11 +82,19 @@ const PAGE_SIZE = 10
 const initFromDate = startOfDay(new Date())
 const initToDate = endOfDay(new Date())
 
-export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem) => void }) {
+export default function GuestsDialog({
+  onChoose
+}: {
+  onChoose: (guest: GuestItem) => void
+}) {
   const [open, setOpen] = useState(false)
   const [fromDate, setFromDate] = useState(initFromDate)
   const [toDate, setToDate] = useState(initToDate)
-  const data: GetListGuestsResType['data'] = []
+  const guestListQuery = useGetGuestListQuery({
+    fromDate,
+    toDate
+  })
+  const data = guestListQuery.data?.payload.data ?? []
   const [sorting, setSorting] = useState<SortingState>([])
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
@@ -128,7 +162,9 @@ export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem
                   placeholder='Từ ngày'
                   className='text-sm'
                   value={format(fromDate, 'yyyy-MM-dd HH:mm').replace(' ', 'T')}
-                  onChange={(event) => setFromDate(new Date(event.target.value))}
+                  onChange={(event) =>
+                    setFromDate(new Date(event.target.value))
+                  }
                 />
               </div>
               <div className='flex items-center'>
@@ -140,21 +176,37 @@ export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem
                   onChange={(event) => setToDate(new Date(event.target.value))}
                 />
               </div>
-              <Button className='' variant={'outline'} onClick={resetDateFilter}>
+              <Button
+                className=''
+                variant={'outline'}
+                onClick={resetDateFilter}
+              >
                 Reset
               </Button>
             </div>
             <div className='flex items-center py-4 gap-2'>
               <Input
                 placeholder='Tên hoặc Id'
-                value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
-                onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
+                value={
+                  (table.getColumn('name')?.getFilterValue() as string) ?? ''
+                }
+                onChange={(event) =>
+                  table.getColumn('name')?.setFilterValue(event.target.value)
+                }
                 className='w-[170px]'
               />
               <Input
                 placeholder='Số bàn'
-                value={(table.getColumn('tableNumber')?.getFilterValue() as string) ?? ''}
-                onChange={(event) => table.getColumn('tableNumber')?.setFilterValue(event.target.value)}
+                value={
+                  (table
+                    .getColumn('tableNumber')
+                    ?.getFilterValue() as string) ?? ''
+                }
+                onChange={(event) =>
+                  table
+                    .getColumn('tableNumber')
+                    ?.setFilterValue(event.target.value)
+                }
                 className='w-[80px]'
               />
             </div>
@@ -168,7 +220,10 @@ export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem
                           <TableHead key={header.id}>
                             {header.isPlaceholder
                               ? null
-                              : flexRender(header.column.columnDef.header, header.getContext())}
+                              : flexRender(
+                                  header.column.columnDef.header,
+                                  header.getContext()
+                                )}
                           </TableHead>
                         )
                       })}
@@ -188,14 +243,20 @@ export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem
                       >
                         {row.getVisibleCells().map((cell) => (
                           <TableCell key={cell.id}>
-                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                            {flexRender(
+                              cell.column.columnDef.cell,
+                              cell.getContext()
+                            )}
                           </TableCell>
                         ))}
                       </TableRow>
                     ))
                   ) : (
                     <TableRow>
-                      <TableCell colSpan={columns.length} className='h-24 text-center'>
+                      <TableCell
+                        colSpan={columns.length}
+                        className='h-24 text-center'
+                      >
                         No results.
                       </TableCell>
                     </TableRow>
@@ -205,8 +266,9 @@ export default function GuestsDialog({ onChoose }: { onChoose: (guest: GuestItem
             </div>
             <div className='flex items-center justify-end space-x-2 py-4'>
               <div className='text-xs text-muted-foreground py-4 flex-1 '>
-                Hiển thị <strong>{table.getPaginationRowModel().rows.length}</strong> trong{' '}
-                <strong>{data.length}</strong> kết quả
+                Hiển thị{' '}
+                <strong>{table.getPaginationRowModel().rows.length}</strong>{' '}
+                trong <strong>{data.length}</strong> kết quả
               </div>
               <div>
                 <AutoPagination

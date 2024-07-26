52e3acef862dc4922ca445d51a2d2e313b1f7e87
Dư Thanh Được <duthanhduoc@gmail.com>
2024-07-26 16:56:47 +0700

    Tích hợp login với google cho next

---
+++ src/app/(public)/(auth)/login/oauth/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { useAppContext } from '@/components/app-provider'
+import { toast } from '@/components/ui/use-toast'
+import { decodeToken, generateSocketInstace } from '@/lib/utils'
+import { useSetTokenToCookieMutation } from '@/queries/useAuth'
+import { useRouter, useSearchParams } from 'next/navigation'
+import { useEffect, useRef } from 'react'
+
+export default function OAuthPage() {
+  const { mutateAsync } = useSetTokenToCookieMutation()
+  const router = useRouter()
+  const count = useRef(0)
+  const { setRole, setSocket } = useAppContext()
+  const searchParams = useSearchParams()
+  const accessToken = searchParams.get('accessToken')
+  const refreshToken = searchParams.get('refreshToken')
+  const message = searchParams.get('message')
+  useEffect(() => {
+    if (accessToken && refreshToken) {
+      if (count.current === 0) {
+        const { role } = decodeToken(accessToken)
+        mutateAsync({ accessToken, refreshToken })
+          .then(() => {
+            setRole(role)
+            setSocket(generateSocketInstace(accessToken))
+            router.push('/manage/dashboard')
+          })
+          .catch((e) => {
+            toast({
+              description: e.message || 'Có lỗi xảy ra'
+            })
+          })
+        count.current++
+      }
+    } else {
+      console.log(message)
+      toast({
+        description: message || 'Có lỗi xảy ra'
+      })
+    }
+  }, [
+    accessToken,
+    refreshToken,
+    setRole,
+    router,
+    setSocket,
+    message,
+    mutateAsync
+  ])
+  return <div />
+}
